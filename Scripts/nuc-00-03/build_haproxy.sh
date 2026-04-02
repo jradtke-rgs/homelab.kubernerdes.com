1196d4e966f95b3f3c92e0edbab9c66c9a451796
jradtke-rgs <jradtke.suse@gmail.com>
2026-04-02 14:19:25 -0400

    Updating to be used with the lab setup

--- Scripts/nuc-00-03/build_haproxy.sh
+++ Scripts/nuc-00-03/build_haproxy.sh
@@ -2,6 +2,12 @@
 
 sudo su -
 
+REPO_SERVER=http://10.0.0.10/
+REPO_NAME=homelab.kubernerdes.com
+REPO_BASE=${REPO_SERVER}${REPO_NAME}
+echo "# NOTE: using $REPO_BASE to pull bits"
+curl ${REPO_BASE}/README.md | tail -2
+
 # Open Ports
 TCP_PORTS="9000 80 443 6443 11434 12000 9345"
 for PORT in $TCP_PORTS
@@ -13,7 +19,7 @@ for PORT in $UDP_PORTS
 do
   firewall-cmd --permanent --add-port=${PORT}/udp
 done
-firewll-cmd --reload
+firewall-cmd --reload
 
 # using Keepalived for floating/VIP (and to future proof)
 zypper -n in haproxy keepalived
@@ -21,12 +27,14 @@ zypper -n in haproxy keepalived
 # Allow keepalive to attach before interface is up/available
 echo "net.ipv4.ip_nonlocal_bind = 1" | sudo tee -a /etc/sysctl.d/20_keepalive.conf
 mv /etc/keepalived/keepalived.conf /etc/keepalived/keepalived.conf.orig
-curl -o  /etc/keepalived/keepalived.conf https://raw.githubusercontent.com/jradtke-rgs/homelab.kubernerdes.com/refs/heads/main/Files/nuc-00-03/etc_keepalived_keepalived.conf
+curl -o /etc/keepalived/keepalived.conf ${REPO_BASE}/Files/nuc-00-03/etc/keepalived/keepalived.conf
+
 sdiff /etc/keepalived/keepalived.conf /etc/keepalived/keepalived.conf.orig
 sudo systemctl enable keepalived --now
 sleep 15; ip a s
 
 cp /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg.$(uuidgen | tr -d '-' | head -c 6)
-curl -o /etc/haproxy/haproxy.cfg https://raw.githubusercontent.com/jradtke-rgs/homelab.kubernerdes.com/refs/heads/main/Files/nuc-00-03/etc_haproxy_haproxy.cfg
+curl -o /etc/haproxy/haproxy.cfg ${REPO_BASE}/Files/nuc-00-03/etc/haproxy/haproxy.cfg
+
 sudo systemctl enable haproxy --now
 
